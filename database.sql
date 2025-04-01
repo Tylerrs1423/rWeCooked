1bde97d3d55fd432f23331c6d8e5afa7b10a05e6
austin-ing <155041167+austin-ing@users.noreply.github.com>
2025-03-31 20:57:37 -0400

    add

--- database.sql
+++ database.sql
@@ -7,4 +7,5 @@ CREATE TABLE UsernamesPasswords(
 );
 
 INSERT INTO UsernamesPasswords (Username, Password)
-VALUES (user1, password1234), (user2, password5678), (user3, password 9012);
+VALUES (user1, password1234), (user2, password5678), (user3, password 9012),
+        (user4, password3456), (user5, password7890), (user6, password123), (user7, password456);
