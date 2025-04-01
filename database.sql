0cfa387ec303ac42f54032cf7e52314c805ba8ed
austin-ing <155041167+austin-ing@users.noreply.github.com>
2025-03-31 20:59:21 -0400

    add

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE UserANDPass;
 USE UsernamesANdPasswords;
 CREATE TABLE UsernamesPasswords(
-    'Username' varchar(50),
-    'Password' varchar(50)
+    Username varchar(50),
+    Password varchar(50)
     PRIMARY KEY (Username)
 );
 
 INSERT INTO UsernamesPasswords (Username, Password)
-VALUES (user1, password1234), (user2, password5678), (user3, password 9012),
+VALUES (user1, password1234), (user2, password5678), (user3, password9012),
         (user4, password3456), (user5, password7890), (user6, password123), (user7, password456);
